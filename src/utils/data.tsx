f6c3d0804140105644a1de7025bebd5dd8f5ac97
Namith Saliyan <chittesaliyan@gmail.com>
2024-08-31 21:33:22 +0530

    Xtend

--- src/utils/data.tsx
+++ src/utils/data.tsx
@@ -1,7 +1,7 @@
 export const bg = require('../assets/images/bgrnd.jpg');
 export const bg2 = require('../assets/images/bgrnd2.jpg');
 export const banner = require('../assets/images/banner.png');
-export const logo = require('../assets/images/logo.jpeg');
+export const logo = require('../assets/images/logoTREAZ.png');
 export const bottles = require('../assets/images/bottles.jpeg');
 export const custombottle = require('../assets/images/custom-bottle.jpeg');
 export const bottlelogo = require('../assets/images/bottlelogo.jpeg');
