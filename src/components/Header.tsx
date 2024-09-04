59d802eb25c78a29e5b63f3baf8ab9110c5da1b4
B M Namith Saliyan <94286020+namithsaliyan@users.noreply.github.com>
2024-09-04 16:26:09 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { logo } from '../utils/data';
 const navigationItems = [
   { name: 'HOME', path: '/' },
   { name: 'PRODUCTS', path: '/products' },
-  { name: 'SERVICES', path: '/#services' },
+  { name: 'SERVICES', path: '/HOME#services' },
   { name: 'TESTIMONIALS', path: '/#testimonials' },
   { name: 'DETAILS', path: '/#details' },
   { name: 'CONTACT', path: '/#contact' }
